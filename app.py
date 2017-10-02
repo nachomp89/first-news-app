46b30eff381e60a1ad73c7f71f8d130f68af3c4d
Nacho <ignaciomorilla@gmail.com>
2017-10-02 15:57:55 +0200

    Created basic table

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import csv
 from flask import Flask
 from flask import render_template #a Flask function we can use to combine data with HTML to make a webpage
 app = Flask(__name__)  # Note the double underscores on each side!
 
+def get_csv():
+	csv_path = './static/la-riots-deaths.csv'
+	csv_file = open(csv_path, 'rb')
+	csv_obj = csv.DictReader(csv_file)
+	csv_list = list(csv_obj)
+	return csv_list
+
 @app.route("/")
 def index(): #this function returns our rendered index.html template
     template = 'index.html'
-    return render_template(template)
+    object_list = get_csv()
+    return render_template(template, object_list=object_list)
 
 if __name__ == '__main__':
     # Fire up the Flask test server
